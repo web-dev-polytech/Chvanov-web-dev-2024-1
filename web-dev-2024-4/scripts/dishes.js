dd76bcafbea730b847f79aca6dff4f5d31778f05
PeaceOfMaster <kirillmaster2020@gmail.com>
2024-11-02 14:22:20 +0300

    Change structure

---
+++ web-dev-2024-4/scripts/dishes.js
@@ -0,0 +1,74 @@
+let dishes = [
+    {
+        keyword: 'gaspacho',
+        name: 'Гаспачо',
+        price: 195,
+        category: 'soup',
+        count: '350 г',
+        image: 'soups/gazpacho.jpg'
+    },
+    {
+        keyword: 'mushroom_soup',
+        name: 'Грибной суп-пюре',
+        price: 185,
+        category: 'soup',
+        count: '330 г',
+        image: 'soups/mushroom_soup.jpg'
+    },
+    {
+        keyword: 'norwegian_soup',
+        name: 'Норвежский суп',
+        price: 270,
+        category: 'soup',
+        count: '330 г',
+        image: 'soups/norwegian_soup.jpg'
+    },
+    {
+        keyword: 'fried_potatoes_with_mushrooms',
+        name: 'Жаренная картошка с грибами',
+        price: 150,
+        category: 'main_course',
+        count: '250 г',
+        image: 'main_course/fried_potatoes_with_mushrooms.jpg'
+    },
+    {
+        keyword: 'lasagna',
+        name: 'Лазанья',
+        price: 385,
+        category: 'main_course',
+        count: '310 г',
+        image: 'main_course/lasagna.jpg'
+    },
+    {
+        keyword: 'chicken_cutlets_with_mashed_potatoes',
+        name: 'Котлеты из курицы с картофельным пюре',
+        price: 225,
+        category: 'main_course',
+        count: '280 г',
+        image: 'main_course/chicken_cutlets_with_mashed_potatoes.jpg'
+    },
+    {
+        keyword: 'orange_juice',
+        name: 'Апельсиновый сок',
+        price: 120,
+        category: 'beverages',
+        count: '300 мл',
+        image: 'beverages/orange_juice.jpg'
+    },
+    {
+        keyword: 'apple_juice',
+        name: 'Яблочный сок',
+        price: 90,
+        category: 'main_course',
+        count: '300 мл',
+        image: 'beverages/apple_juice.jpg'
+    },
+    {
+        keyword: 'carrot_juice',
+        name: 'Морковный сок',
+        price: 110,
+        category: 'beverage',
+        count: '300 мл',
+        image: 'beverages/carrot_juice.jpg'
+    }
+];
