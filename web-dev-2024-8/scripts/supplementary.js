1f813b74daebd61c32fd4026fec0e6f9b949f1f7
PeaceOfMaster <kirillmaster2020@gmail.com>
2024-12-02 15:00:13 +0300

    Laba 8 init commit

---
+++ web-dev-2024-8/scripts/supplementary.js
@@ -0,0 +1,24 @@
+export const category_names_dictionary = {
+    'soup': 'суп',
+    'main-course': 'главное блюдо',
+    'salad': 'салат или стартер',
+    'drink': 'напиток',
+    'dessert': 'десерт'
+};
+export const kinds_dictionary = {
+    'veg': 'вегетарианское',
+    'meat': 'мясное',
+    'fish': 'рыбное',
+    'cold': 'холодный',
+    'hot': 'горячий',
+    'small': 'маленькая порция',
+    'medium': 'средняя порция',
+    'large': 'большая порция'
+};
+export const category_order = [
+    'soup',
+    'main-course',
+    'salad',
+    'drink',
+    'dessert'
+];
